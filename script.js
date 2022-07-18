d89636b1dd81d1e890973cfad33a3fb73b9f0565
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-07-18 12:17:21 +0300

    adding delete names feature

--- script.js
+++ script.js
@@ -9,19 +9,16 @@ const main=document.querySelector("main");
 const loader=document.querySelector(".loader");
 const namesContainer=document.querySelector(".namesContainer");
 
-
 inputBox.addEventListener("keyup",function(evt){
     let {value}=evt.target;
     value=value.replace(/\s/g, '');
     evt.target.value=value;
 });
-
 function setAttribute(element,attributes){
     for(let attribute in attributes){
         element.setAttribute(attribute,attributes[attribute])
     }
 }
-
 function createCountriesElem(countries){
     countries.forEach(country => {
         const countryContainer=document.createElement('div');
@@ -48,22 +45,18 @@ function createCountriesElem(countries){
 form.addEventListener("submit",async function(evt){
     evt.preventDefault();
     const name=inputBox.value.toLowerCase();
-    
     const names=JSON.parse(localStorage.getItem("names"));
     if(names && !names.includes(name) || names==null){
         const newNames=names?[...names,name]:[name];
         localStorage.setItem("names",JSON.stringify(newNames));
     }
     loadNames();
-
     main.classList.toggle("hidden");
     loader.classList.toggle("hidden");
     flagsCont.replaceChildren([]);
     const {gender,nationality,age}=await getNameDate(name);
-    
     setAttribute(genderPic,{src:`./images/${gender.gender}.png`,alt:gender.gender});
     ageText.textContent=`Age : ${age.age}`;
-
     try{
         const countries=await getCountiresData(nationality.country);
         createCountriesElem(countries);
@@ -74,13 +67,25 @@ form.addEventListener("submit",async function(evt){
     }
 });
 
+function deleteName(evt){
+    const name=evt.target?.id;
+    const names=JSON.parse(localStorage.getItem("names"));
+    const index=names.indexOf(name);
+    const newNames=names.slice(0,index).concat(names.slice(index+1));
+    localStorage.setItem("names",JSON.stringify(newNames));
+    loadNames();
+}
+
 function loadNames(){
     const names=JSON.parse(localStorage.getItem("names"));
     names && namesContainer.replaceChildren([]);
     names && names.forEach(name=>{
         const nameElem=document.createElement("h3");
         nameElem.textContent=name;
+        setAttribute(nameElem,{class:"SavedName",id:name});
+        nameElem.addEventListener("click",deleteName);
         namesContainer.append(nameElem);
     });
 }
 loadNames();
+
